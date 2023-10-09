c6b3fd4e1f96b98389acff07e10dfdd63733fca4
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2023-10-09 10:58:08 -0400

    Create Ex6.java

---
+++ Ex6.java
@@ -0,0 +1,27 @@
+/* Make a program that reads any value and displays a message saying in which of the following intervals ([0.25], (25.50), (50.75), (75.100]) this value is found. Obviously, if the value is not in any of these ranges, the message "Out of range" should be printed. */
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insert a double value: ");
+        double num = sc.nextDouble();
+
+        if (num < 0.0 || num > 100.0) {
+            System.out.println("Out of range");
+        } else if (num <= 25.0) {
+            System.out.println("Interval [0,25]");
+        } else if (num <= 50.0) {
+            System.out.println("Interval (25,50]");
+        } else if (num <= 75.0) {
+            System.out.println("Interval (50,75]");
+        } else {
+            System.out.println("Interval (75,100]");
+        }
+
+        sc.close();
+    }
+}
