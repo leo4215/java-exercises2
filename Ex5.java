d378fbe2b0e6b30f55c704d5e41ef0d033a5e57a
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2023-10-09 10:50:15 -0400

    Create Ex5.java

---
+++ Ex5.java
@@ -0,0 +1,42 @@
+/* Based on the table below, write a program that reads the code and the quantity of that item. Next, calculate and show the total.
+   * CODE - PRODUCT - PRICE
+   * 1    -   a     - $ 4.00
+   * 2    -   b     - $ 4.50
+   * 3    -   c     - $ 5.00
+   * 4    -   d     - $ 2.00
+   * 5    -   e     - $ 1.50
+*/
+
+import java.util.Scanner;
+
+public class Ex5 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insert the code: ");
+        int code = sc.nextInt();
+        System.out.println("Insert the amount: ");
+        int amount = sc.nextInt();
+
+        double total;
+
+        if (code == 1) {
+            total = amount * 4.0;
+        } else if (code == 2) {
+            total = amount * 4.5;
+        } else if (code == 3) {
+            total = amount * 5.0;
+        } else if (code == 4) {
+            total = amount * 2.0;
+        } else if (code == 5) {
+            total = amount * 1.5;
+        } else {
+            System.out.println("ERROR - code unrecognized");
+        }
+
+        System.out.printf("Total: $ %.2f%n", total);
+
+        sc.close();
+    }
+}
