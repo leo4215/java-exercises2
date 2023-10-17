4a4793983ddc54e9b41184ef3a5b6de35529d018
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2023-10-17 10:54:17 -0400

    Create Ex1.java

--- Ex1.java
+++ Ex1.java
@@ -7,7 +7,7 @@ public class Ex1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Insert an integer:");
+        System.out.println("Enter an integer:");
         int N = sc.nextInt();
 
         if (N < 0) {
