db598cc18070129c3de330dbc484fd18eea3d8a0
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2023-10-17 10:56:45 -0400

    Create Ex6.java

--- Ex6.java
+++ Ex6.java
@@ -7,7 +7,7 @@ public class Ex6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Insert a double value:");
+        System.out.println("Enter a double value:");
         double num = sc.nextDouble();
 
         if (num < 0.0 || num > 100.0) {
